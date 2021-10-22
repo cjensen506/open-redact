0e99d9199e36a37b659afdefbd4d41e4058e8aa4
cjensen506 <chris.jensen506@gmail.com>
2021-10-22 16:45:27 -0700

    partial api unit tests, failing unittest

---
+++ app/test/test_api.py
@@ -0,0 +1,22 @@
+from fastapi.testclient import TestClient
+
+from app.main.main import app
+
+client = TestClient(app)
+
+
+def test_post_json():
+    response = client.post(
+        "/redact_pdf",
+        json={"id": "foobar", "title": "Foo Bar", "description": "The Foo Barters"}
+    )
+    assert response.status_code == 422
+
+
+def test_post_wrong_file_type():
+    with open("./app/test/sample_txt_file.txt", "rb") as file:
+        response = client.post(
+            "/redact_pdf",
+            data={"file": file}
+        )
+        assert response.status_code == 415
